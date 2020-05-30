a3d5cc6500372f3c7f2ecb17148f43d522ec6914
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-30 00:32:28 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -128,3 +128,142 @@ def game_hash
 end
 
 # Write code here
+
+# knows the number of points scored by each player (FAILED - 1)
+def num_points_scored(player)
+  game_hash.each do |k, team|
+    team[:players].each do |e|
+      if e[:player_name] == player
+        return e[:points]
+      end
+    end
+  end
+end
+
+# knows the shoe size of each player (FAILED - 2)
+def shoe_size(player)
+  game_hash.each do |k, team|
+    team[:players].each do |e|
+      if e[:player_name] == player
+        return e[:shoe]
+      end
+    end
+  end
+end
+
+# knows the Brooklyn Nets colors are Black and White (FAILED - 3)
+# knows the Charlotte Hornets colors are Turquoise and Purple (FAILED - 4)
+def team_colors(user_team_name)
+  game_hash.each do |k, team|
+    if team[:team_name] == user_team_name
+      return team[:colors]
+    end
+  end
+end
+
+# returns the team names (FAILED - 5)
+def team_names
+  names = []
+  game_hash.each do |k, team|
+    names.push(team[:team_name])
+  end
+  names
+end
+
+# returns the player jersey numbers (FAILED - 6)
+def player_numbers(user_team_name)
+  numbers = []
+  game_hash.each do |k, team|
+    if team[:team_name] == user_team_name
+      team[:players].each do |player|
+        numbers.push(player[:number])
+      end
+    end
+  end
+  numbers
+end
+
+# returns all stats for a given player (FAILED - 7)
+def player_stats(player)
+  game_hash.each do |k, team|
+    team[:players].each do |e|
+      if e[:player_name] == player
+        return e
+      end
+    end
+  end
+end
+
+# returns the number of rebounds of the player with the biggest shoe size (FAILED - 8)
+def big_shoe_rebounds
+  biggest_shoe_size = 0
+  player_rebounds = 0
+  game_hash.each do |k, team|
+    team[:players].each do |e|
+      if e[:shoe] > biggest_shoe_size
+        biggest_shoe_size = e[:shoe]
+        player_rebounds = e[:rebounds]
+      end
+    end
+  end
+  player_rebounds
+end
+
+
+# ------ BONUS -------
+
+# Which player has the most points?
+def most_points_scored
+  most_points = 0
+  player = ""
+  game_hash.each do |k, team|
+    team[:players].each do |e|
+      if e[:points] > most_points
+        most_points = e[:points]
+        player = e[:player_name]
+      end
+    end
+  end
+  player
+end
+
+
+# Which team has the most points?
+def winning_team
+  home_team_points = game_hash[:home][:players].sum { |e| e[:points] }
+  away_team_points = game_hash[:away][:players].sum { |e| e[:points] }
+
+  if home_team_points > away_team_points
+    return game_hash[:home][:team_name]
+  else
+    return game_hash[:away][:team_name]
+  end
+end
+
+# Which player has the longest name?
+def player_with_longest_name
+  longest_name = ""
+  game_hash.each do |k, team|
+    team[:players].each do |e|
+      if e[:player_name].length > longest_name.length
+        longest_name = e[:player_name]
+      end
+    end
+  end
+  longest_name
+end
+
+def long_name_steals_a_ton?
+  most_steals = 0
+  player_with_most_steals = ""
+  game_hash.each do |k, team|
+    team[:players].each do |e|
+      if e[:steals] > most_steals
+        most_steals = e[:steals]
+        player_with_most_steals = e[:player_name]
+      end
+    end
+  end
+
+  player_with_longest_name() == player_with_most_steals ? true : false
+end
